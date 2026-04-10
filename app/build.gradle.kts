2dd74ed89dcce3c33dcdee022596df4618901a61
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2026-04-10 10:46:38 +0700

    Downgrade Material Components library version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ configurations.all {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     
     // Javalin for HTTP server with reliable streaming support
